62e0afeceff20c6e2add2cdbbb3d58efeb72a2b6
Dom <dominic.garguilo@gmail.com>
2021-03-23 21:55:37 -0400

    data get request

--- server.js
+++ server.js
@@ -2,8 +2,8 @@
 // there is a ton of information about it online and its really easy to use.
 
 // Turning this js script into an express app
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 const port = process.env.PORT || 3000;
 
 // Use these files as static files (meaning send these to the user as-is to their browser)
@@ -14,16 +14,21 @@ app.use(express.static(__dirname + '/public'));
 app.get('/', (req, res) => {
   console.log("Loading index");
   res.sendFile(__dirname + '/index.html');
-})
+});
 
 
 // try it with a different suffix! go to http://localhost:3000/ping
 app.get('/ping', (req, res) => {
   console.log("Incoming GET request");
   res.json('pong');
-})
+});
+
+app.get('/data', (req, res) => {
+  console.log("GET data");
+  res.sendFile(__dirname + '/public/data/data.json');
+});
 
 // listen at specified port
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
-})
+});
