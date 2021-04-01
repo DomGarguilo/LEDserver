1cac94232f101cda411364b924f0f46072168f34
Dom <dominic.garguilo@gmail.com>
2021-03-31 21:55:41 -0400

    Gets json image data from server and displays it on screen

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,1 +1,65 @@
-document.write("Hello world from javascript");
+console.log('in scripts');
+
+displayImagePreview();
+
+//inserts css into stylesheet
+function addCss(cssCode) {
+    var styleElement = document.createElement("style");
+    if (styleElement.styleSheet) {
+        styleElement.styleSheet.cssText = cssCode;
+    } else {
+        styleElement.appendChild(document.createTextNode(cssCode));
+    }
+    document.getElementsByTagName("head")[0].appendChild(styleElement);
+}
+
+//fetches the image data json from server
+async function getJsonData() {
+    var response = await fetch(document.URL + 'data');
+    //var response = await fetch('https://led-matrix-server.herokuapp.com/')
+    jsonData = await response.json();
+    return jsonData;
+}
+// generates the css color array for a single frame of animation
+function generateFrame(pixelSize, frameRange, data) {
+    var result = frameRange + ' {box-shadow:';
+    for (var i = 1; i <= 16; i++) {
+        for (var j = 1; j <= 16; j++) {
+            result += (pixelSize * i).toString() + 'px ' + (pixelSize * j).toString() + 'px 0 0 #' + data[i - 1][j - 1];
+            if (j == 16 && i == 16) {
+                result += ';';
+            } else {
+                result += ',';
+            }
+        }
+    }
+    result += 'height:' + pixelSize.toString() + 'px;width:' + pixelSize.toString() + 'px;}';
+    result = result.replaceAll('0x', '');
+    return result;
+}
+//wraps the color array with additional CSS for displaying the frame
+function generateFrameSet(pixelSize, frameRange, data) {
+    var result = '@keyframes x {' + generateFrame(pixelSize, frameRange, data) + '}';
+    return result;
+}
+
+function displayImagePreview() {
+    getJsonData().then((v) => {
+        var arr = get2Darray(v.animationList[0].frames[0]);
+        var cssArray = generateFrameSet(40, '0%, 100%', arr);
+        addCss(cssArray);
+    });
+
+}
+
+function get2Darray(arr) {
+    var result = Array.from(Array(16), () => new Array(16));
+    var i;
+    for (i = 0; i < 16; i++) {
+        var j;
+        for (j = 0; j < 16; j++) {
+            result[i][j] = arr[(j * 16) + i];
+        }
+    }
+    return result;
+}
--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 // there is a ton of information about it online and its really easy to use.
 
 // Turning this js script into an express app
+//const { json } = require('express');
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
@@ -12,6 +13,9 @@ var path = require('path')
 // Use these files as static files (meaning send these to the user as-is to their browser)
 //app.use(express.static(__dirname + '/public'));
 app.use(express.static(path.join(__dirname, 'public')));
+
+// favicon setup
+app.use('/favicon.ico', express.static(__dirname + 'public/images/favicon.ico'));
 
 // when the user makes a get request to '/' (meaning http://{website}/ -- but in our case, http://localhost:3000/)
 // send them the file `FILENAME`
@@ -31,11 +35,16 @@ app.get('/ping', (req, res) => {
 
 app.get('/data', (req, res) => {
   console.log("GET data");
-  var data = fs.readFileSync(__dirname + '/public/data/data.json', 'utf8');
-  res.json(JSON.parse(data));
+  res.json(getJsonData());
 });
 
 // listen at specified port
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
+
+//update jsonData variable from file
+function getJsonData() {
+  console.log('reading json file');
+  return JSON.parse(fs.readFileSync(__dirname + '/public/data/data.json', 'utf8'));
+}
